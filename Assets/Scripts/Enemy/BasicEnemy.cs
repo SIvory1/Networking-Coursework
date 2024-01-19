50534557d0726bbc4b42beb42c9bf2e8f542dd3f
SIvory1 <SIVORY200@caledonian.ac.uk>
2024-01-19 22:48:18 +0000

    seek wip

--- Assets/Scripts/Enemy/BasicEnemy.cs
+++ Assets/Scripts/Enemy/BasicEnemy.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
-using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class BasicEnemy : NetworkBehaviour
 {
@@ -12,14 +10,23 @@ public class BasicEnemy : NetworkBehaviour
     [SerializeField] private float speed;
     private Vector2 enemyPos;
     Animator animator;
-    public enum EnemyState { patrol, seek};
+    Rigidbody2D rb;
 
+    public enum EnemyState { patrol, seek};
     public EnemyState enemyState;
+
+    GameObject[] _player;
+    private float[] distanceFromPlayer;
 
     private void Start()
     {
+        _player = GameObject.FindGameObjectsWithTag("Player");
+        print(_player.Length);
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         enemyState = EnemyState.patrol;
+        distanceFromPlayer = new float[_player.Length];
+
     }
 
     // sync when they join
@@ -34,10 +41,56 @@ public class BasicEnemy : NetworkBehaviour
                 break;
             case EnemyState.seek:
                 {
-
+                    FindClosestPlayer();
+                    Seek();
                 }
                 break;
+        }   
+
+    }
+
+    Vector2 desiredVelocity;
+    Vector3 steeringVelocity;
+    public void Seek()
+    {
+        // Get the desired velocity for seek and limit to maxSpeed
+        desiredVelocity = Vector3.Normalize(closetPlayer.transform.position - transform.position) * speed;
+
+        // Calculate steering velocity
+        steeringVelocity = desiredVelocity - rb.velocity;
+
+        transform.position += steeringVelocity * Time.deltaTime;
+    }
+
+    GameObject closetPlayer;
+
+    void FindClosestPlayer()
+    {
+        for (int i = 0; i < _player.Length; i++)
+        {
+            if (_player[i] != null)
+            {
+                distanceFromPlayer[i] = (_player[i].transform.position-transform.position).magnitude;
+            }
         }
+
+        if (_player.Length > 1)
+        {
+            if (distanceFromPlayer[0] < distanceFromPlayer[1])
+            {
+                closetPlayer = _player[0];
+            }
+            else
+            {
+                closetPlayer = _player[1];
+            }
+        }
+        else
+        {
+            closetPlayer = _player[0];
+        }
+ 
+        //float min = distanceFromPlayer.Min();    
     }
 
     [ClientRpc]
